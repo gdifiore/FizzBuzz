e2deaf2707ebccdad73dba5359d3d86e9fd65ce1
Gabe <itzg3bz@gmail.com>
2018-01-13 20:59:03 -0500

    Create FizzBuzz.js

---
+++ FizzBuzz.js
@@ -0,0 +1,25 @@
+/*
+ *
+ * FizzBuzz.js - FizzBuzz
+ *
+ * (c) gdifiore 2018 
+ *      
+ */
+
+function fizz_buzz(num)
+{
+    if (num % 15 == 0) {
+        console.log("FizzBuzz");
+    } else if (num % 5 == 0) {
+        console.log("Buzz");
+    } else if (num % 3 == 0) {
+        console.log("Fizz");
+    } else {
+        console.log(num);
+    }
+}
+
+for (var i = 1; i <= 100; i++)
+{
+    fizz_buzz(i);
+}
